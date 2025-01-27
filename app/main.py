ea67fbf99f1cdb2d0fea92b072553c94116ab4eb
Rasheed <relsaleh@gmail.com>
2025-01-26 21:02:33 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 import sys
-import re
 
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
 
 def match_pattern(input_line, pattern):
-    if re.search(pattern, input_line):
-        return True
+    if len(pattern) == 1:
+        return pattern in input_line
+    elif pattern == "\\d":
+        return any(c.isdigit() for c in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
