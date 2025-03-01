10106a63969d4acd5bab7689a4faeb868234972b
Rasheed <relsaleh@gmail.com>
2025-02-28 23:57:55 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,43 +4,60 @@ import sys
 # import lark - available if you need it!
 
 
-def match(group_start_idx, original_input_line, input_line, pattern):
+def match(original_input_line, input_line, pattern):
     if pattern[0] == "^":
-        return match_pattern(
-            group_start_idx, original_input_line, input_line, pattern[1:]
-        )
+        return match_pattern(original_input_line, input_line, pattern[1:])
     for i in range(len(input_line)):
-        if match_pattern(group_start_idx, original_input_line, input_line[i:], pattern):
+        if match_pattern(original_input_line, input_line[i:], pattern):
             return True
     return False
 
 
-def match_plus(group_start_idx, original_input_line, input_line, pattern, pattern_idx):
+def match_plus(original_input_line, input_line, pattern, pattern_idx):
     # if pattern[0] != "." and input_line[0] != pattern[0]:
     #     return False
 
-    return match_pattern(
-        group_start_idx, original_input_line, input_line[1:], pattern
-    ) or match_pattern(
-        group_start_idx, original_input_line, input_line[1:], pattern[pattern_idx:]
+    return match_pattern(original_input_line, input_line[1:], pattern) or match_pattern(
+        original_input_line,
+        input_line[1:],
+        pattern[pattern_idx:],
     )
 
 
-groups = [""]
+group_str = {}
+groups = []
 
 
-def match_pattern(group_start_idx, original_input_line, input_line, pattern):
+def match_pattern(original_input_line, input_line, pattern):
+    # print(f"input_line: {input_line}, pattern:{pattern}")
     if not pattern:
         return True
 
     if not input_line:
-        return True if pattern in ["$", ")"] else False
+        return pattern in ["$", ")"]
 
     if pattern[0] == ")":
+        # print(f"pattern: {pattern}, input_line:{input_line}")
+        # print(f"original: {original_input_line}, input_line: {input_line}")
         group_end_idx = len(original_input_line) - len(input_line)
-        groups.append(original_input_line[group_start_idx:group_end_idx])
+        # print(f"groups before pop: {groups}")
+        # print(f"pattern: {pattern}, input_line:{input_line}")
+        group_number, group_start_idx = groups.pop()
+        # print(f"groups after pop: {groups}")
+
+        # print(
+        #     f"group_number: {group_number}, group_start_idx: {group_start_idx}, group_end_idx: {group_end_idx}"
+        # )
+        # print(
+        #     f"group_number: {group_number}, start: {group_start_idx}, end: {group_end_idx}"
+        # )
+        # print(f"pattern: {original_input_line[group_start_idx:group_end_idx]}")
+        group_str[group_number] = original_input_line[group_start_idx:group_end_idx]
+        # print(f"group_str: {group_str}")
         return match_pattern(
-            group_start_idx, original_input_line, input_line, pattern[1:]
+            original_input_line,
+            input_line,
+            pattern[1:],
         )
 
     if pattern[0] == "(":
@@ -50,12 +67,17 @@ def match_pattern(group_start_idx, original_input_line, input_line, pattern):
             if pattern[i] == ")":
                 break
 
+        group_number = len(group_str) + 1
+        # print(f"group_number: {group_number}")
+        group_str.setdefault(group_number, "")
         group_start_idx = len(original_input_line) - len(input_line)
+        groups.append((group_number, group_start_idx))
+        # print(f"groups append: {groups}")
+        # print(f"groups after append: {groups}")
 
         if "|" in pattern[1:right_paren_idx]:
             for group_pattern in pattern[1:right_paren_idx].split("|"):
                 if match_pattern(
-                    group_start_idx,
                     original_input_line,
                     input_line,
                     group_pattern + pattern[right_paren_idx:],
@@ -63,16 +85,17 @@ def match_pattern(group_start_idx, original_input_line, input_line, pattern):
                     return True
         else:
             if match_pattern(
-                group_start_idx, original_input_line, input_line, pattern[1:]
+                original_input_line,
+                input_line,
+                pattern[1:],
             ):
                 return True
 
         return False
 
     if pattern[0] == "\\" and pattern[1].isdigit():
-        group_pattern = groups[int(pattern[1])]
+        group_pattern = group_str[int(pattern[1])]
         return match_pattern(
-            group_start_idx,
             original_input_line,
             input_line,
             group_pattern + pattern[2:],
@@ -81,39 +104,51 @@ def match_pattern(group_start_idx, original_input_line, input_line, pattern):
     if len(pattern) > 1 and pattern[1] == "+":
         if pattern[0] != "." and input_line[0] != pattern[0]:
             return False
-        return match_plus(group_start_idx, original_input_line, input_line, pattern, 2)
+        return match_plus(original_input_line, input_line, pattern, 2)
 
     if len(pattern) > 1 and pattern[1] == "?":
         if input_line[0] == pattern[0]:
             return match_pattern(
-                group_start_idx, original_input_line, input_line[1:], pattern[2:]
+                original_input_line,
+                input_line[1:],
+                pattern[2:],
             )
 
-        return match_pattern(
-            group_start_idx, original_input_line, input_line, pattern[2:]
-        )
+        return match_pattern(original_input_line, input_line, pattern[2:])
 
     if pattern[0] == "." or pattern[0] == input_line[0]:
         return match_pattern(
-            group_start_idx, original_input_line, input_line[1:], pattern[1:]
+            original_input_line,
+            input_line[1:],
+            pattern[1:],
         )
 
     if pattern[:2] == "\\d" and input_line[0].isdigit():
         if len(pattern) > 2 and pattern[2] == "+":
             return match_plus(
-                group_start_idx, original_input_line, input_line, pattern, 3
+                original_input_line,
+                input_line,
+                pattern,
+                3,
             )
         return match_pattern(
-            group_start_idx, original_input_line, input_line[1:], pattern[2:]
+            original_input_line,
+            input_line[1:],
+            pattern[2:],
         )
 
     if pattern[:2] == "\\w" and (input_line[0].isalnum() or input_line[0] == "_"):
         if len(pattern) > 2 and pattern[2] == "+":
             return match_plus(
-                group_start_idx, original_input_line, input_line, pattern, 3
+                original_input_line,
+                input_line,
+                pattern,
+                3,
             )
         return match_pattern(
-            group_start_idx, original_input_line, input_line[1:], pattern[2:]
+            original_input_line,
+            input_line[1:],
+            pattern[2:],
         )
 
     if pattern[0] == "[":
@@ -123,20 +158,18 @@ def match_pattern(group_start_idx, original_input_line, input_line, pattern):
             if pattern[i] == "]":
                 break
         if pattern[1] == "^":
-            if input_line[0] not in pattern[2:right_bracket_idx]:
+            if input_line[0] not in pattern[2:right_bracket_idx] + ",":
                 if (
                     right_bracket_idx < len(pattern) - 1
                     and pattern[right_bracket_idx + 1] == "+"
                 ):
                     return match_plus(
-                        group_start_idx,
                         original_input_line,
                         input_line,
                         pattern,
                         right_bracket_idx + 2,
                     )
                 return match_pattern(
-                    group_start_idx,
                     original_input_line,
                     input_line[1:],
                     pattern[right_bracket_idx + 1 :],
@@ -148,14 +181,12 @@ def match_pattern(group_start_idx, original_input_line, input_line, pattern):
                     and pattern[right_bracket_idx + 1] == "+"
                 ):
                     return match_plus(
-                        group_start_idx,
                         original_input_line,
                         input_line,
                         pattern,
                         right_bracket_idx + 2,
                     )
                 return match_pattern(
-                    group_start_idx,
                     original_input_line,
                     input_line[1:],
                     pattern[right_bracket_idx + 1 :],
@@ -175,9 +206,11 @@ def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
     # Uncomment this block to pass the first stage
-    if match(0, input_line, input_line, pattern):
+    if match(input_line, input_line, pattern):
+        print("match found")
         exit(0)
     else:
+        print("match not found")
         exit(1)
 
 
